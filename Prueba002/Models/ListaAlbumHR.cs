b7ff7bf27f7b89dbf63ccfcbc464e53b70087e69
diana.ovaldes.13@gmail.com <diana.ovaldes.13@gmail.com>
2023-11-27 22:04:49 -0600

    funcionamiento de todossss los cruds con diseño

---
+++ Prueba002/Models/ListaAlbumHR.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Prueba002.Models
+{
+    public class ListaAlbumHR
+    {
+        public int IdLista { get; set; }
+        public int IdAlbum { get; set; }
+        public DateTime FechaAgregado { get; set; }
+    }
+}
